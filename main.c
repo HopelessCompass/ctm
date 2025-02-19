e9f3c066a7d20753845710d1456ab9d8a53bb3e6
Kristina <31845754+HopelessCompass@users.noreply.github.com>
2025-02-19 11:35:34 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,17 @@
 
 int main(void)
 {
+    //variables
+    char cmd[10]
+
+    //welcome screen
     printf("Console Task Manager\n");
     printf("Enter 'List' to get a list of tasks\n");
+    printf("Enter 'Kill' to enter killing process menu\n");
+    printf("Enter 'Pause' to enter pause process menu\n");
+    printf("Enter 'Unpause' to enter unpause process menu\n");
+
+    //enter command request
+    scanf("%s", &cmd);
     return 0;
 }
