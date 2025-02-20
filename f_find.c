223f0072479cf90c74d821236dc82fdcb69ea765
Kristina <31845754+HopelessCompass@users.noreply.github.com>
2025-02-20 18:14:06 +0300

    Фукнция поиска процессе

--- f_find.c
+++ f_find.c
@@ -36,7 +36,12 @@ int getPIDbyProcName(const char * procname) {
 }
 
 int main(void) {
-    int pid = getPIDbyProcName("notepad.exe");
-    printf("Notepad %s%d)\n", pid > 0 ? "found at PID: (" : "NOT FOUND (", pid);
+
+    char process_name[20];
+    printf("Enter a process name to look for\n");
+    scanf("%10s", process_name);
+
+    int pid = getPIDbyProcName(process_name);
+    printf("Process %s%d)\n", pid > 0 ? "found at PID: (" : "NOT FOUND (", pid);
     return 0;
 }
