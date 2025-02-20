eb63d62bac0753c2b45c1d53dcd636c151155c4e
Kristina <31845754+HopelessCompass@users.noreply.github.com>
2025-02-20 20:02:40 +0300

    Вынесение функции сканирования в отдельный файл

--- f_find.c
+++ f_find.c
@@ -1,39 +1,4 @@
-#include <windows.h>
 #include <stdio.h>
-#include <psapi.h>
-
-int getPIDbyProcName(const char * procname) {
-    int pid = 0;
-    DWORD Procs[1024], bytesReturned, NumOfProcesses;
-    TCHAR szProcessName[MAX_PATH];
-
-    // Get the list of process identifiers.
-    if ( !EnumProcesses(Procs, sizeof(Procs), &bytesReturned) )
-        return 0;
-
-    // Calculate how many process identifiers were returned.
-    NumOfProcesses = bytesReturned / sizeof(DWORD);
-
-    for (int i = 0; i < NumOfProcesses; i++ ) {
-        if (Procs[i] != 0) {
-            // Get a handle to the process.
-            HANDLE hProc = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, Procs[i]);
-            // and find the one we're looking for
-            if (hProc != NULL) {
-                HMODULE hModule;
-                if (EnumProcessModules(hProc, &hModule, sizeof(hModule), &bytesReturned)) {
-                    GetModuleBaseName(hProc, hModule, (LPSTR) szProcessName, sizeof(szProcessName)/sizeof(TCHAR));
-                    if (lstrcmpiA(procname, szProcessName) == 0) {
-                        pid = Procs[i];
-                        break;
-                    }
-                }
-            }
-            CloseHandle(hProc);
-        }
-    }
-    return pid;
-}
 
 int main(void) {
 
