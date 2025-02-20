732841f66eaa1bc1c4dc5b9f7741dd9dd8fb16c6
Kristina <31845754+HopelessCompass@users.noreply.github.com>
2025-02-20 20:06:56 +0300

    Откатила назад f_list

--- f_list.c
+++ f_list.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main(void) {
- int pid = getPIDbyProcName("notepad.exe");
- printf("Notepad %s%d)\n", pid > 0 ? "found at PID: (" : "NOT FOUND (", pid);
- return 0;
+    char process_name[20];
+    printf("Enter a process name to look for\n");
+    scanf("%10s", process_name);
+
+    int pid = getPIDbyProcName(process_name);
+    printf("Process %s%d)\n", pid > 0 ? "found at PID: (" : "NOT FOUND (", pid);
+    return 0;
 }
